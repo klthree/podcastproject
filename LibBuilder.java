da069214d0089421f433b9544081c8db9a54d44c
klthree <klthree@users.github.com>
2018-09-09 00:10:57 -0400

    build file with feed urls

---
+++ LibBuilder.java
@@ -0,0 +1,53 @@
+package podcastproject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.util.Scanner;
+
+public class LibBuilder {
+    private String feedUrl;
+
+    public LibBuilder (String feedUrl) {
+        this.feedUrl = feedUrl + "\n";
+    }
+
+//    public void downloadFile () {
+//        try {
+//        BufferedWriter bw = new BufferedWriter(new FileWriter("feedLib", true));
+//         
+//        bw.write(feedUrl + "\n");
+//        bw.close();
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static void main (String[] args) {
+//        Scanner keyboard = new Scanner(System.in);
+//        System.out.print("Enter feed url: ");
+//        String input = keyboard.nextLine();
+//
+//        StartPoint sp = new StartPoint(input);
+//        sp.downloadFile();
+//    }
+        public void download() {
+        try {
+        BufferedWriter bw = new BufferedWriter(new FileWriter("feedLib", true));
+        bw.write(feedUrl);
+        bw.close();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+        }
+    public static void main (String[] args) {
+        Scanner keyboard = new Scanner (System.in);
+        System.out.print("Enter url: ");
+        String input = keyboard.nextLine();
+        LibBuilder lb = new LibBuilder(input);
+            lb.download();
+    }
+}
